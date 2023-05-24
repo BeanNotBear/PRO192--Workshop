1eca2c40836b80114a45c259830a63c5412e38e4
BeanNotBear <132796382+BeanNotBear@users.noreply.github.com>
2023-05-24 09:16:04 +0700

    Update Part1.java

--- Workshop1/src/workshop1/part1/Part1.java
+++ Workshop1/src/workshop1/part1/Part1.java
@@ -126,19 +126,6 @@ public class Part1 {
         }
     }
     
-    static void insertionSort(int[] arr) {
-        int length = arr.length;
-        for(int i = 1; i < length; i++) {
-            int tmp = arr[i];
-            int j = i - 1;
-            while(j >= 0 && arr[j] < tmp) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = tmp;
-        }
-    }
-    
 }
 
     
