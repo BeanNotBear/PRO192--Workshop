d21de181423ac99e798f03c9e657c7ae756e8195
BeanNotBear <132796382+BeanNotBear@users.noreply.github.com>
2023-06-03 21:58:37 +0700

    Update Painting.java

--- Workshop3/src/workshop3/Painting.java
+++ Workshop3/src/workshop3/Painting.java
@@ -71,16 +71,38 @@ public class Painting extends Item {
 
     public void inputPainting() {
         Scanner sc = new Scanner(System.in);
+        super.input();
         while (true) {
             try {
                 System.out.print("Enter height: ");
                 this.height = Integer.parseInt(sc.nextLine());
+                while(this.height < 0) {
+                    System.out.println("Height can not be a negative number, pls enter again:");
+                    this.height = Integer.parseInt(sc.nextLine());
+                }
                 System.out.print("Enter width: ");
                 this.width = Integer.parseInt(sc.nextLine());
+                while(this.width < 0) {
+                    System.out.println("Width can not be a negative number, pls enter again:");
+                    this.width = Integer.parseInt(sc.nextLine());
+                }
+                String pattern = "^(true|false|True|False)$";
                 System.out.print("Is water colour? ");
-                this.isWaterColour = sc.nextBoolean();
+                String ans = sc.nextLine();
+                this.isWaterColour = Boolean.parseBoolean(ans);
+                while(!ans.matches(pattern)) {
+                    System.out.println("Incorrect datatype, pls enter true or false:");
+                    ans = sc.nextLine();
+                    this.isWaterColour = Boolean.parseBoolean(ans);
+                }
                 System.out.print("Is framed? ");
-                this.isFramed = sc.nextBoolean();
+                ans = sc.nextLine();
+                this.isFramed = Boolean.parseBoolean(ans);
+                while(!ans.matches(pattern)) {
+                    System.out.println("Incorrect datatype, pls enter true or false:");
+                    ans = sc.nextLine();
+                    this.isFramed = Boolean.parseBoolean(ans);
+                }
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Incorrect datatype, pls enter again");
